d40b15c09debbceef2406fd20f1bc4997c881e70
League Student <league@iMac-14.attlocal.net>
2019-11-29 18:28:00 -0800

    module0 done

---
+++ src/_06_everything_is_awesome/EverythingIsAwsome.java
@@ -0,0 +1,15 @@
+package _06_everything_is_awesome;
+
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwsome {
+	public static void main(String[] args) {
+		
+		String awesome = JOptionPane.showInputDialog(null, "What do YOU think is awsome???");
+				JOptionPane.showMessageDialog(null, awesome + " is awsome");
+		
+	}
+	
+	
+		
+}
---
+++ src/_07_last_summer/LastSummer.java
@@ -0,0 +1,11 @@
+package _07_last_summer;
+
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog(null, "What is your name??");
+		JOptionPane.showMessageDialog(null, "I know that you farted last summer, " + name + ". HaHaHa!!");
+	}
+
+}
